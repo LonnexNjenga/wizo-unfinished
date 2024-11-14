be81c98813993af874a07d60b30cff20e707a02d
LonnexNjenga <134331160+LonnexNjenga@users.noreply.github.com>
2024-11-14 18:11:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+/*
+
+const toogleBtn = document.querySelector('.toogle-btn')
+const toogleBtnIcon = document.querySelector('.toogle-btn img')
+const dropdownMenu = document.querySelector('.dropdown-menu')
+
+toogleBtn.onclick = function (){
+    dropdownMenu.classList.toggle('open')
+
+}
+*/
+
+
+
+
+
+
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
